04cce2244fae7228404290a7c41efe456d832e7e
RizalMifta <rizalmifta79@gmai.com>
2020-04-15 15:14:32 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} 
+from "react-router-dom";
+import Header from "./Header";
+import Blog from "./Blog";
+import BlogDetail from "./BlogDetail";
+
+//install bootstrap dulu
+// import 'bootstrap/dist/css/bootstrap.min.css';git 
+
+// Ini merupakan functional component
+// https://getbootstrap.com/docs/4.4/examples/navbar-static/
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/about">About</Route>
+          <Route path="/blog/:articleId" component={BlogDetail} />
+          <Route path="/blog" component={Blog}><Blog /></Route>
+          <Route path="/">Home</Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
