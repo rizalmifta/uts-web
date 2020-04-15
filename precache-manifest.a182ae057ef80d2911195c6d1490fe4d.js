f2775f458260a483a551b97fc04b5fd88bf95b07
RizalMifta <rizalmifta79@gmai.com>
2020-04-15 15:19:56 +0700

    Updates

---
+++ precache-manifest.a182ae057ef80d2911195c6d1490fe4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70dd85c8bcfcde802564883f827a85df",
+    "url": "/uts-web/index.html"
+  },
+  {
+    "revision": "b91b1498e0f6f5c774ea",
+    "url": "/uts-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51d51b751428cf41452d",
+    "url": "/uts-web/static/js/2.bbc3203f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uts-web/static/js/2.bbc3203f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91b1498e0f6f5c774ea",
+    "url": "/uts-web/static/js/main.8756129d.chunk.js"
+  },
+  {
+    "revision": "ca801146ba426346990a",
+    "url": "/uts-web/static/js/runtime-main.7a03afa5.js"
+  }
+]);
